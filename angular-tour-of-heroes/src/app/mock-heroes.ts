9e72f4ff42c3efc96c66b4815f57f828f07d5016
LaChance <Martha.LaChance@Pearson.com>
2019-06-06 16:24:57 -0400

    step2

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Hairy Larry' },
+  { id: 12, name: 'Mr. Awesome' },
+  { id: 13, name: 'Snow Cone' },
+  { id: 14, name: 'Dr. Hurdle' },
+  { id: 15, name: 'Ice Princess' },
+  { id: 16, name: 'Twizzler Man' },
+  { id: 17, name: 'Pasta King' },
+  { id: 18, name: 'Mr. Cheese' },
+  { id: 19, name: 'Gray Rhino' },
+  { id: 20, name: 'Wolf Bear' }
+];
